b71a4f225c131cb3adaabbe80898594d2b31e9e9
Lee Blecher <ldb78@cornell.edu>
2015-08-09 15:45:00 -0400

    Changed some roofing pictures and made roofing page

---
+++ roofing.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html>
+
+<head>
+	<title>Blake Windows, Siding, and Roofing</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css"/>
+	<link rel="stylesheet" type="text/css" href="css/slider.css"/>
+	<link rel="stylesheet" type="text/css" href="css/demo.css"/>
+	<link rel="stylesheet" type="text/css" href="css/elastislide.css"/>
+	<link rel="stylesheet" type="text/css" href="css/featherlight.min.css"/>
+	<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700' rel='stylesheet' type='text/css'>
+	<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+	<link rel="icon" href="images/favicon.png" type="image/x-icon">
+
+	<script id="img-wrapper-tmpl" type="text/x-jquery-tmpl">	
+		<div class="rg-image-wrapper">
+			{{if itemsCount > 1}}
+				<div class="rg-image-nav">
+					<a href="#" class="rg-image-nav-prev">Previous Image</a>
+					<a href="#" class="rg-image-nav-next">Next Image</a>
+				</div>
+			{{/if}}
+			<div class="rg-image-boundary">
+			<div class="rg-image"></div>
+			</div>
+			<div class="rg-loading"></div>
+		</div>
+	</script>
+
+</head>
+
+<body>
+	<div id="resttopbar">
+		<a href="index.html"><img id="logo" src="images/logo.png"></a>
+		<div id="navbar">
+			<div id="numbers">1-877-BLAKE-OK / 516-488-4858 / 718-766-8600</div><br>
+			<ul class="list">
+				<li class="line"><a class="shadow" href="info.php">ABOUT</a></li>
+				<li class="line"><a class="shadow" href="roofing.html">ROOFING</a></li>
+				<li class="line"><a class="shadow" href="siding.html">SIDING</a></li>
+				<li class="line"><a class="shadow" href="windows.html">WINDOWS</a></li>
+				<li class="line"><a class="shadow" href="contact.php">GUTTERS</a></li>
+				<li class="line"><a class="shadow" href="contact.php">OTHER SERVICES</a></li>
+			</ul>
+		</div><br>
+	</div>
+	<div id="mainroofingpic">
+		<!-- <img id="gafroof" src="images/gaf-roof-new.jpg"> -->
+		<h1 id="restmaintext">ROOFING</h1>
+	</div>
+	<div class="innercontent">
+		<div id="sidebyside">
+			<div class="leftside">
+				<p class="descript">We offer peace of mind when you install your roof.  As a GAF Master Elite Ceritified Residential Roofing Contractor (since 2001) we use top quality roofing materials, give you a professional roof installation and give you the best transferrable warranties.
+				<br><br>Your roof can represent up to 40% of your home's curb appeal
+				<br><br>Choosing the style and color of your roof will greatly enhance the look of your home. GAF Architectural Timberline Roofing Shingles and GAF Architectural Designer Roofing Shingles offer a large variety of colors to choose from.
+</p>
+			</div>
+			<div class="rightside">
+				<p id="righttitle">Request a Free Estimate!</p>
+				<form class="sideform">
+						<input type="text" name="name" class="sideput" id="name" placeholder="Name*"><br>
+						<input type="text" name="address" class="sideput" id="address" placeholder="Address"><br>
+						<input type="text" name="city" class="sideput" id="city" placeholder="City*"><br>
+						<input type="text" name="zip" class="sideput" id="zip" placeholder="Zip Code*"><br>
+						<input type="text" tabindex="3" name="number" class="sideput" id="number" placeholder="Phone Number*"><br>
+						<input type="email" name="email" class="sideput" id="email" placeholder="Email*"><br>
+						<textarea id="sidemessage" placeholder="Message*"></textarea>
+					<br><br>
+					<input type="submit" id="sidesub" name="submit">
+				</form>
+			</div>
+		</div>
+		<div id="rg-gallery" class="rg-gallery">
+			<div class="rg-thumbs">
+				<!-- Elastislide Carousel Thumbnail Viewer -->
+				<div class="es-carousel-wrapper">
+					<div class="es-nav">
+						<span class="es-nav-prev">Previous</span>
+						<span class="es-nav-next">Next</span>
+					</div>
+					<div class="es-carousel">
+						<ul>
+							<li><a href="#"><img src="images/thumbs/1.jpg" data-large="images/roofing/afteronlybayside.jpg" alt="image01" data-description="GAF Slateline Antique Gray Roofing in Bayside" /></a></li>
+							<li><a href="#"><img src="images/thumbs/ferdinandafter.jpg" data-large="images/roofing/afteronlybayside2.jpg" alt="image02" data-description="GAF Timberline Charcoal Roofing in Bayside" /></a></li>
+							<li><a href="#"><img src="images/thumbs/3.jpg" data-large="images/roofing/afteronlycambria.jpg" alt="image03" data-description="GAF Timberline Pewter Gray Roofing in Cambria Heights" /></a></li>
+							<li><a href="#"><img src="images/thumbs/4.jpg" data-large="images/roofing/afteronlycambria2.jpg" alt="image04" data-description="GAF Timberline Fox Hollow Gray Roofing in Cambria Heights" /></a></li>
+							<li><a href="#"><img src="images/thumbs/5.jpg" data-large="images/siding/farrockawaycropped.jpg" alt="image05" data-description="Before and after in Far Rockaway" /></a></li>
+							<li><a href="#"><img src="images/thumbs/6.jpg" data-large="images/siding/flushing1cropped.jpg" alt="image06" data-description="Before and after in Flushing" /></a></li>
+							<li><a href="#"><img src="images/thumbs/7.jpg" data-large="images/siding/flushing2cropped.jpg" alt="image07" data-description="Before and after in Flushing" /></a></li>
+							<li><a href="#"><img src="images/thumbs/8.jpg" data-large="images/siding/glenheadcropped.jpg" alt="image08" data-description="Before and after in Glen Head" /></a></li>
+							<li><a href="#"><img src="images/thumbs/9.jpg" data-large="images/siding/holliscropped.jpg" alt="image09" data-description="Before and after in Hollis" /></a></li>
+							<li><a href="#"><img src="images/thumbs/10.jpg" data-large="images/siding/nhpcropped.jpg" alt="image10" data-description="Before and after in New Hyde Park" /></a></li>
+							<li><a href="#"><img src="images/thumbs/11.jpg" data-large="images/siding/nobeforebayside.jpg" alt="image11" data-description="Siding in Bayside" /></a></li>
+							<li><a href="#"><img src="images/thumbs/12.jpg" data-large="images/siding/nobeforewhitestone.jpg" alt="image12" data-description="Siding in Whitestone" /></a></li>
+							<li><a href="#"><img src="images/thumbs/13.jpg" data-large="images/siding/nobeforewhitestone2.jpg" alt="image13" data-description="Side of house in Whitestone" /></a></li>
+							<li><a href="#"><img src="images/thumbs/14.jpg" data-large="images/siding/nobeforenhp.jpg" alt="image14" data-description="Siding in New Hyde Park" /></a></li>
+							<li><a href="#"><img src="images/thumbs/15.jpg" data-large="images/siding/nobeforerosedale.jpg" alt="image15" data-description="Siding in Rosedale" /></a></li>
+							<li><a href="#"><img src="images/thumbs/16.jpg" data-large="images/siding/nobeforehollis.jpg" alt="image16" data-description="Siding in Hollis" /></a></li>
+							<li><a href="#"><img src="images/thumbs/17.jpg" data-large="images/siding/saintalbanscropped.jpg" alt="image17" data-description="Siding in Saint Albans" /></a></li>
+							<li><a href="#"><img src="images/thumbs/18.jpg" data-large="images/siding/syossetcropped.jpg" alt="image18" data-description="Siding in Syosset" /></a></li>
+							<li><a href="#"><img src="images/thumbs/19.jpg" data-large="images/siding/sidingonlycambria.jpg" alt="image19" data-description="Siding in Cambria Heights" /></a></li>
+							<li><a href="#"><img src="images/thumbs/20.jpg" data-large="images/siding/sidingonlyhollis.jpg" alt="image20" data-description="Siding in Hollis" /></a></li>
+							<li><a href="#"><img src="images/thumbs/21.jpg" data-large="images/siding/sidingonlylakesuccess.jpg" alt="image21" data-description="Siding in Lake Success" /></a></li>
+							<li><a href="#"><img src="images/thumbs/22.jpg" data-large="images/siding/sidingonlylakesuccess2.jpg" alt="image22" data-description="Siding on Back of House in Lake Success" /></a></li>
+							<li><a href="#"><img src="images/thumbs/23.jpg" data-large="images/siding/sidingonlywindowheaders.jpg" alt="image23" data-description="Siding and Window Headers" /></a></li>
+							<!-- <li><a href="#"><img src="images/thumbs/24.jpg" data-large="images/24.jpg" alt="image24" data-description="As they did battery to the spheres intend" /></a></li> -->
+						</ul>
+					</div>
+				</div>
+				<!-- End Elastislide Carousel Thumbnail Viewer -->
+			</div><!-- rg-thumbs -->
+		</div><!-- rg-gallery -->
+	</div>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery.tmpl.min.js"></script>
+	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="js/jquery.elastislide.js"></script>
+	<script type="text/javascript" src="js/gallery.js"></script>
+	<script type="text/javascript" src="js/featherlight.min.js"></script>
+	<script src="js/jquery.maskedinput.js"></script>
+	<script src="js/script.js"></script>
+
+
+</body>
+
+</html>
